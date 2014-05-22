94e72311c799ce34460037447f174123185ed1ec
LandonWest <landonwest5@gmail.com>
2014-05-22 16:00:52 -0600

    added answer dialog

--- numbers_fun.rb
+++ numbers_fun.rb
@@ -2,4 +2,5 @@ puts "Give me a number."
 number_one = gets.chomp.to_i
 puts "Now give me another number."
 number_two = gets.chomp.to_i
+puts "These numbers add up to equal:"
 puts number_one + number_two
