e3e99f9d32ba21a46b3ed84484dbe3b6e4313a94
LandonWest <landonwest5@gmail.com>
2014-05-23 14:28:20 -0600

    adds more choices to the calculator

--- numbers_fun.rb
+++ numbers_fun.rb
@@ -1,9 +1,20 @@
 puts "Give me a number."
-number_one = gets.chomp.to_i
+number_one = gets.chomp.to_f
 puts "Now give me another number."
-number_two = gets.chomp.to_i
-puts "These numbers add up to equal:"
-puts number_one + number_two
+number_two = gets.chomp.to_f
 
+puts "Would you like to add, subtract, divide, or multiply them?"
+choice = gets.chomp.downcase
 
-puts "Hello"
+case choice
+when "add"
+  puts "Your answer is: #{number_one} + #{number_two}"
+when "subtract"
+  puts "Your answer is: #{number_one} + #{number_two}"
+when "divide"
+  puts "Your answer is: #{number_one} + #{number_two}"
+when "multiply"
+  puts "Your answer is: #{number_one} + #{number_two}"
+else
+  puts "something's not right here"
+end
