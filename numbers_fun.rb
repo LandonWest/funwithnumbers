ad0be19ff306313fed6e91e396cb493e0de59ef7
LandonWest <landonwest5@gmail.com>
2014-05-23 23:44:24 -0600

    add opening line, loop, and quit function.

--- numbers_fun.rb
+++ numbers_fun.rb
@@ -1,20 +1,32 @@
-puts "Give me a number."
-number_one = gets.chomp.to_f
-puts "Now give me another number."
-number_two = gets.chomp.to_f
+calculator = true
+while calculator == true
 
-puts "Would you like to add, subtract, divide, or multiply them?"
-choice = gets.chomp.downcase
+  puts '"I\'m good with calculation!"'
 
-case choice
-when "add"
-  puts "Your answer is: #{number_one} + #{number_two}"
-when "subtract"
-  puts "Your answer is: #{number_one} + #{number_two}"
-when "divide"
-  puts "Your answer is: #{number_one} + #{number_two}"
-when "multiply"
-  puts "Your answer is: #{number_one} + #{number_two}"
-else
-  puts "something's not right here"
+  puts "Give me a number."
+  number_one = gets.chomp.to_f
+  puts "Now give me another number."
+  number_two = gets.chomp.to_f
+
+  puts "Would you like to add, subtract, divide, or multiply them?"
+  choice = gets.chomp.downcase
+
+  case choice
+  when "add"
+    puts "Your answer is: #{number_one + number_two}"
+  when "subtract"
+    puts "Your answer is: #{number_one - number_two}"
+  when "divide"
+    puts "Your answer is: #{number_one / number_two}"
+  when "multiply"
+    puts "Your answer is: #{number_one * number_two}"
+  else
+    puts "something's not right here"
+  end
+
+  puts "Do you want to continue?"
+  continue = gets.chomp.downcase
+
+  exit if continue == "no"
+
 end
